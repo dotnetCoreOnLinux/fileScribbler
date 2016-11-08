489ecdc8ff4a013109554b45b8870e4e0705f2fc
josephantoney <josephantoney@gmail.com>
2016-11-07 22:26:17 -0500

    win10 and CentOS
    code to write and read on multi platform

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace fileTopgSQL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateReadFile();
+        }
+        /*
+        Method      :   CreateReadFile
+        Description :   Method to create/read dummy file on any OS 
+        */
+        public static void CreateReadFile()
+        {
+            try{
+                string path = System.Reflection.Assembly.GetEntryAssembly().Location;
+                path = path.Substring(0,path.LastIndexOf(Path.DirectorySeparatorChar));
+                path = path + Path.DirectorySeparatorChar + DateTime.Now.Ticks.ToString();
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Scribble");
+                    sw.WriteLine("on");
+                    sw.WriteLine("any Platform");
+                }
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Something went wrong :(");
+                Console.WriteLine("Here is the StackTrace:" + ex.StackTrace);
+            }
+        }
+    }
+}
